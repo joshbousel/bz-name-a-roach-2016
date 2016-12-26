711242492a182c4cf3d1f0df0a8fe13b0cb3228e
Josh Bousel <joshbousel@WCSs-MacBook-Pro-3.local>
2016-12-26 11:16:51 -0600

    Added initial styles for eCard support

--- js/name-a-roach.js
+++ js/name-a-roach.js
@@ -3,5 +3,10 @@ Y.use("jquery-ui", function(Y) {
 		jQuery('#tribute_honoree_namename').maxlength({ maxCharacters: 22, slider: true }); 
 		jQuery('#tribute_honoree_first_namename').maxlength({ maxCharacters: 30, slider: true }); 
 		jQuery('#tribute_message_bodyname').maxlength({ maxCharacters: 220, slider: true }); 
+	
+		if (jQuery("#taf_send").length) {
+			jQuery("#taf_send").attr('value', 'Send');
+			jQuery("#taf_popup_preview").attr('value', 'Preview');
+		}
 	});
 });
