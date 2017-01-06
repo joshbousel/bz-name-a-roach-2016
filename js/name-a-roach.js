06090b84cb937016ea732c920c2815f5ed8afe3a
Joshua Bousel <josh@bousel.com>
2017-01-05 20:58:33 -0500

    Style and JS changes for 2017

--- js/name-a-roach.js
+++ js/name-a-roach.js
@@ -3,5 +3,10 @@ Y.use("jquery-ui", function(Y) {
 		jQuery('#tribute_honoree_namename').maxlength({ maxCharacters: 22, slider: true }); 
 		jQuery('#tribute_honoree_first_namename').maxlength({ maxCharacters: 30, slider: true }); 
 		jQuery('#tribute_message_bodyname').maxlength({ maxCharacters: 220, slider: true }); 
+		
+		if (jQuery('#ecard_send_date_MONTH').length) {
+			jQuery('#ecard_send_date_MONTH').val('2');
+			jQuery('#ecard_send_date_DAY').val('13');
+		}
 	});
 });
