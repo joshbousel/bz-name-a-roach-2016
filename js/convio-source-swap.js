a680850237a3e04a496c897a032204c62e39a138
Joshua Bousel <josh@bousel.com>
2017-01-18 15:43:19 -0500

    Added source swapping script for roach landing page

---
+++ js/convio-source-swap.js
@@ -0,0 +1,25 @@
+var $ = require('jquery');
+
+$(function(){
+	$.urlParam = function(name){
+		var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+		
+		if (results==null){
+			return null;
+		} else {
+			return results[1] || 0;
+		}
+	}
+	
+	$('.ken-burns-hover a').each(function(){
+		var anchor = $(this);
+		var url = anchor.attr('href');
+		var src = $.urlParam('s_src');
+		var subsrc = $.urlParam('s_subsrc');
+		
+		url = src != null ? url.replace('s_src=CAMP_Name-a-Roach','s_src='+src) : url;
+		url = subsrc != null ? url.replace('s_subsrc=CAMP_Name-a-Roach','s_src='+subsrc) : url;
+			
+		anchor.attr('href',url);
+	});
+});
